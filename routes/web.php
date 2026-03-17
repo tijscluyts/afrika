169384c2139ba0ff103f0142bc434bd7a8e472fe
tijscluyts <r0983582@student.thomasmore.be>
2026-03-17 11:26:07 +0100

    live map

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use App\Http\Controllers\FarmingController;
 
 // Dit is de enige route die je nodig hebt voor de homepage
 Route::get('/', [FarmingController::class, 'index']);
+
+// Route for the farm map simulation
+Route::get('/farm-map', function () {
+    return view('farm-map');
+});
